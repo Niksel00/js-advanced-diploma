224cf5f45b0727f810244e6734de01247e9a2ff4
Niksel00 <nikita-efremov-00@mail.ru>
2021-07-21 12:56:09 +0400

    Retro RPG Game v.1.0

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,5 +1,39 @@
 export function calcTileType(index, boardSize) {
-  // TODO: write logic here
+  if (index % 8 === 0) {
+    // first column -> first line
+    if (index < boardSize) {
+      return 'top-left';
+    }
+    // first column -> last line
+    if (index >= boardSize ** 2 - boardSize) {
+      return 'bottom-left';
+    }
+    // else first column
+    return 'left';
+  }
+
+  if (index % 8 === 7) {
+    // last column -> first line
+    if (index < boardSize) {
+      return 'top-right';
+    }
+    // last column -> last line
+    if (index >= boardSize ** 2 - boardSize) {
+      return 'bottom-right';
+    }
+    // else last column
+    return 'right';
+  }
+
+  // center of first line
+  if (index < boardSize) {
+    return 'top';
+  }
+  // center of last line
+  if (index >= boardSize ** 2 - boardSize) {
+    return 'bottom';
+  }
+  // all the rest
   return 'center';
 }
 
